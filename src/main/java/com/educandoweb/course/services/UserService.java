5500271da154b0fafdf14bca7602c51e7cc817bb
uaidri <uaidri@gmail.com>
2021-02-11 11:32:09 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -17,11 +17,15 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-	Optional<User> obj = repository.findById(id);
-	return obj.get();
-		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 
 }
